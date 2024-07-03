c1d89bfc22d35d6edf2f5f0efe372d216f433ffe
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2024-07-03 10:50:04 -0300

    feat: initial commit

---
+++ portifolio-arthur-pelegrino/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./global.scss";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Arthur Pelegrino",
+  description: "Arthur Pelegrino is a software engineer full stack with a lot of skills",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
