514f1652ff63830cc123e3244ef1c38fa4449c8e
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2024-07-04 18:39:46 -0300

    Versão inicial finalizada, preciso refatorar as experiences e continuar adicionando os meus projetos

--- portifolio-arthur-pelegrino/app/components/experience/experience.tsx
+++ portifolio-arthur-pelegrino/app/components/experience/experience.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import { SectionTitle } from "../sectionTitle/section-title"
 import "./experience.scss"
 
@@ -5,10 +6,64 @@ export function Experience() {
     return(
         <div className="experience">
             <SectionTitle text="Experience"/>
-            <p>Texto</p>
             <div className="experience-time">
-                
-            </div>  
+                <div className="experience-language">
+                    <Image
+                        src="/react.png"
+                        alt="React Logo"
+                        width={40}
+                        height={40}
+                        priority
+                    />
+                    <div className="experience-unit">
+                        <div className="experience-measure measure-1">
+                            <span> 1 years </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="experience-language">
+                    <Image
+                        src="/ts.png"
+                        alt="Type Script Logo"
+                        width={40}
+                        height={40}
+                        priority
+                    />
+                    <div className="experience-unit">
+                        <div className="experience-measure measure-1">
+                        <span> 1 year </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="experience-language">
+                    <Image
+                        src="/js.png"
+                        alt="Java Script Logo"
+                        width={40}
+                        height={40}
+                        priority
+                    />
+                    <div className="experience-unit">
+                        <div className="experience-measure measure-3">
+                            <span> 2 years </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="experience-language">
+                    <Image
+                        src="/java.png"
+                        alt="Java Logo"
+                        width={40}
+                        height={40}
+                        priority
+                    />
+                    <div className="experience-unit">
+                        <div className="experience-measure measure-1">
+                            <span> 1 year </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
